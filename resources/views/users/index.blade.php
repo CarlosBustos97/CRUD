11d885fb15ed63f4bbc6c28bdb2940c35fd62c57
Carlos Bustos <carlosbustos1997@gmail.com>
2021-07-02 10:41:05 -0500

    crud terminado con la tabla usuarios

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -19,10 +19,38 @@
             }
         </style>
     </head>
-    <body class="antialiased">
+    <body>
         <div class="container">
             <div class="row">
                 <div class="col-sm-8 mx-auto">
+                    <div class="card border-0 shadow">
+                        <div class="card-body ">
+                            @if ($errors->any())
+                                <div class="alert alert-danger">
+                                    @foreach ($errors->all() as $error)
+                                        -{{$error}} <br>
+                                    @endforeach
+                                </div>
+                            @endif
+                            <form action="{{route('users.store')}}" method="POST">
+                                <div class="form-group row">
+                                    <div class="col-sm-3">
+                                        <input type="text" name="name" class="form-control" placeholder="Nombre" value="{{old('name')}}">
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <input type="text" name="email" class="form-control" placeholder="Email" value="{{old('email')}}">
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <input type="password" name="password" class="form-control" placeholder="Contraseña">
+                                    </div>
+                                    <div class="col-auto">
+                                        @csrf
+                                        <button type="submit" class="btn btn-primary"> Enviar </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>                      
+                    </div>
                     <table class="table">
                         <thead>
                             <tr>
@@ -38,7 +66,19 @@
                                     <td>{{$user->id}}</td>
                                     <td>{{$user->name}}</td>
                                     <td>{{$user->email}}</td>
-                                    <td>Eliminar</td>
+                                    <td>
+                                        <form action="{{route('users.destroy', $user)}}" method="POST">
+                                            @method('DELETE')
+                                            @csrf
+                                            <input 
+                                                type="submit" 
+                                                value="eliminar" 
+                                                class="btn btn-sm btn-danger" 
+                                                onclick="return confirm('Desea eliminar')"
+                                            >
+
+                                        </form>
+                                    </td>
                                 </tr>
                             @endforeach
                         </tbody>
